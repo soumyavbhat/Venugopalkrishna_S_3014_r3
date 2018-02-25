654d65953ff01650499aee76b2cf7d0f79ebab7f
Soumya Venugopalkrishna <s_venugopalkrishna@fanshaweonline.ca>
2018-02-24 23:51:26 -0500

    Finished files

---
+++ admin/phpscripts/config.php
@@ -0,0 +1,10 @@
+<?php
+	require_once('functions.php');
+	require_once('login.php');
+	require_once('sessions.php');
+	require_once('user.php');
+	// require_once('encrypt.php');
+	require_once('mail.php');
+	require_once('query.php');
+
+?>
